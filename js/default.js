0b45a5dc8b45023a002b10db06b7078a32bb85f1
iago-pl <iagopenalorenzo@gmail.com>
2022-12-16 15:10:01 +0100

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,15 +1,19 @@
 window.onload = function () {
-    var main = document.getElementById("main");
+    try {
+        var main = document.getElementById("main");
 
-    main.style.backgroundPosition = "center -40000px";
+        main.style.backgroundPosition = "center -40000px";
 
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function () {
-        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/637b3a15b0d6371309d02fcf/1gicllpld';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function () {
+            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/637b3a15b0d6371309d02fcf/1gicllpld';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    } catch (error) {
+        console.log("Tawk not loaded");
+    }
 }
