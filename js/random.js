5303a35de08d185bbc53bca68c4ec3e335b6b91c
iago-pl <iagopenalorenzo@gmail.com>
2022-12-21 16:00:54 +0100

    Update random.js

--- js/random.js
+++ js/random.js
@@ -3,8 +3,9 @@ const vignette = document.getElementById("overlay");
 const steps = 10;
 
 const defaultSize = 1;
-const finalSize = 1.5;
+const finalSize = 1.4;
 const disp = 5;
+const endTime = 1.5;
 
 var item;
 var time = 0;
@@ -14,6 +15,7 @@ var interval;
 var populationPlaces;
 
 var onSeq = false;
+var onZoom = false;
 vignette.style.filter = "opacity(0%)";
 
 load();
@@ -33,36 +35,50 @@ function sequence() {
         return;
     }
     onSeq = true;
+    onZoom = true;
     button.style.display = "none";
     waitToTime = time + seqDuration * steps;
     size = defaultSize;
     interval = setInterval(updateTime, 1000 / steps);
 }
 
-function endSeq() {
-    clearInterval(interval);
-    time = 0;
+function endZoom() {
 
     switchItem();
-    item.ref.style.transform = `scale(  ${defaultSize}  ) translate(0px, 0px) rotate(0deg)`;
+    item.ref.style.transform = `scale(  ${finalSize + 0.25}  ) translate(0px, 0px) rotate(0deg)`;
 
     button.style.display = "block";
-    onSeq = false;
-    vignette.style.filter = "opacity(0%)";
-    vignette.style.backdropFilter = `blur(0px)`;
+    onZoom = false;
 }
 
 function updateTime() {
     time++;
-    if (time >= waitToTime) {
+    if (time >= waitToTime && onZoom) {
+        endZoom();
+    }
+
+    if (time >= waitToTime + (endTime * 10)) {
         endSeq();
         return;
     }
-    switchItem();
-    size += (finalSize - defaultSize) / (steps * seqDuration);
-    item.ref.style.transform = `scale(  ${size}  ) translate(${((time / disp) / 2) - (Math.random() * (time / disp))}px, ${((time / disp) / 2) - (Math.random() * (time / disp))}px) rotate(${((time / disp) / 2) - (Math.random() * (time / disp))}deg) `;
-    vignette.style.filter = `opacity(${100 - (100 - time * 2)}%)`;
-    vignette.style.backdropFilter = `blur(${time / 10}px)`;
+
+    if (onZoom) {
+        switchItem();
+        size += (finalSize - defaultSize) / (steps * seqDuration);
+        item.ref.style.transform = `scale(  ${size}  ) translate(${((time / disp) / 2) - (Math.random() * (time / disp))}px, ${((time / disp) / 2) - (Math.random() * (time / disp))}px) rotate(${((time / disp) / 2) - (Math.random() * (time / disp))}deg) `;
+        vignette.style.filter = `opacity(${100 - (100 - time * 2)}%)`;
+        vignette.style.backdropFilter = `blur(${time / 10}px)`;
+    }
+
+}
+
+function endSeq() {
+    clearInterval(interval);
+    time = 0;
+    onSeq = false;
+    vignette.style.filter = "opacity(0%)";
+    vignette.style.backdropFilter = `blur(0px)`;
+    item.ref.style.transform = `scale(  ${defaultSize}  ) translate(0px, 0px) rotate(0deg)`;
 
 }
 
